41eae45c851104d90c4cdedd89afd49e1ec1ac12
Corey H <c.l.halicke@gmail.com>
2019-02-12 02:23:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,11 @@
 def oxford_comma(array)
-  'if array.length = 1 
+  if array.length = 1 
     array.join
   elsif array.length = 2
     array.join(" and ")
   elsif array.length > 2 
-    last = array.last'
-    print array.pop
+    last = array.last
+    array.pop
+    array.join(" , ") + " and " last
   
 end
