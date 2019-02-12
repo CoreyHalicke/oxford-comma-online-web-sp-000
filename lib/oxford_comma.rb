3738af592a0d3b767fc92e180865d2c2c36f7a25
Corey H <c.l.halicke@gmail.com>
2019-02-12 02:22:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,10 @@
 def oxford_comma(array)
-  array.join(" and ")
+  'if array.length = 1 
+    array.join
+  elsif array.length = 2
+    array.join(" and ")
+  elsif array.length > 2 
+    last = array.last'
+    print array.pop
   
 end
